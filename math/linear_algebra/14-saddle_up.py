d3159b92f2ca7139f81593897d6e60ee8b2c8f17
ulrichr7 <ulrichskyler7@gmail.com>
2025-10-08 17:57:20 +0200

    alu-machine_learning

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,18 +1,12 @@
-    #!/usr/bin/env python3
-"""Module to perform matrix multiplication using NumPy."""
-
+#!/usr/bin/env python3
+"""
+Module `14-saddle_up`
+"""
 import numpy as np
 
 
 def np_matmul(mat1, mat2):
     """
-    Perform matrix multiplication between two numpy arrays.
-
-    Args:
-        mat1 (numpy.ndarray): First matrix.
-        mat2 (numpy.ndarray): Second matrix.
-
-    Returns:
-        numpy.ndarray: Result of matrix multiplication.
+    Perform a matrix multiplication
     """
     return np.matmul(mat1, mat2)
